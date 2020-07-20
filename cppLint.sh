877e3e7425977d92df48a9778412ba20f41f615f
liuxiang2019 <liuxiang2019@sogou-inc.com>
2020-07-20 22:57:25 +0800

    Add cppLint.sh's robustness

--- cppLint.sh
+++ cppLint.sh
@@ -5,13 +5,19 @@ set -eu
 THIS_SCRIPT_PATH=$(cd "$(dirname "$0")"; pwd)
 
 cppLint() {
-    echo "~~~ Start CppLint In $THIS_SCRIPT_PATH ~~~"
+    cppFiles=$(find $THIS_SCRIPT_PATH -name '*.h' -or -name '*.c' -or -name '*.cpp')
+    # Without "", $cppFiles may be split out into multiple words
+    # For more information, 
+    # please see "https://stackoverflow.com/questions/13781216/meaning-of-too-many-arguments-error-from-if-square-brackets"
+    if [ -z "$cppFiles" ];then
+      echo "$THIS_SCRIPT_PATH has no cpp files!"
+      exit
+    fi
     cpplint --counting=detailed \
             --filter=-legal/copyright \
             --quiet \
-            $( find $THIS_SCRIPT_PATH -name \*.h -or -name \*.c -or -name \*.cpp) \
-            2>&1
-    echo "~~~ CppLint Done ~~~"
+            $cppFiles \
+            2>$THIS_SCRIPT_PATH/cppLint_output.txt
 }
 
 cppLint
