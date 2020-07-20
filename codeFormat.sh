f32241f446faaa413acf902abffef310d96856e7
liuxiang <sphinxs@vip.qq.com>
2020-07-20 18:46:00 +0800

    Refine format

--- codeFormat.sh
+++ codeFormat.sh
@@ -7,12 +7,12 @@ CLANG_FORMAT_CONG_FILE=".clang-format"
 
 # Check whether the .clang-format file exists in current path
 if [ ! -f $CLANG_FORMAT_CONG_FILE ];then
-	echo "$THIS_SCRIPT_PATH Has No .clang-format!"
-	exit 
+    echo "$THIS_SCRIPT_PATH Has No .clang-format!"
+    exit 
 fi
 
 format() {
-	echo "~~~ Start Code Format In $THIS_SCRIPT_PATH ~~~"
+    echo "~~~ Start Code Format In $THIS_SCRIPT_PATH ~~~"
     for f in $(find $THIS_SCRIPT_PATH -name '*.h' -or -name '*.c' -or -name '*.cpp'); do 
         echo "format ${f}"
         clang-format -i -style=file ${f}
