84b31e3342c98b3c7659d7dd23efd638ac6c514a
liuxiang2019 <liuxiang2019@sogou-inc.com>
2020-07-21 18:06:06 +0800

    Add python script for process cpplint output

---
+++ python/CppLintParser/cppLint.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+import argparse
+from cppLintParser import Parser
+
+def parseArgs():
+    argParser = argparse.ArgumentParser(description="Process cpplint output")
+    argParser.add_argument('-wd', dest='wd', help='Cpp Project Path')
+    argParser.add_argument('-sh', dest='script', help='CppLint.sh\'s Path')
+    argParser.add_argument('-in', dest='input', help='CppLintParser\'s input file')
+    argParser.add_argument('-o', dest='output', help='CppLint File Output Path')
+    args = argParser.parse_args()
+    return args
+
+# 执行cppLint.sh
+def runShellScript(shellScript):
+    if os.path.exists(shellScript):
+        subprocess.Popen(shellScript, shell=True)
+    else:
+        print("Here is no %s !" % (shellScript))
+        assert False
+
+def processShellScriptOuput(inputFile, outPutPath):
+    if os.path.exists(inputFile):
+        cppLintparser = Parser()
+        cppLintparser.parse(inputFile)
+        if not os.path.exists(outPutPath):
+            os.makedirs(outPutPath)
+        cppLintparser.output(outPutPath)
+    else:
+        print("Here is no %s !" % (inputFile))
+        assert False
+
+if __name__ == '__main__':
+    args = parseArgs()
+    runShellScript(args.script)
+    processShellScriptOuput(args.input, args.output)
+    
---
+++ python/CppLintParser/cppLintParser.py
@@ -0,0 +1,90 @@
+import os
+
+class CppLintError:
+    def __init__(self, cppLintLine):
+        assert cppLintLine
+        # location start from 1
+        self.location = 0 
+        self.category = ""
+        self.description = ""
+        # verbose:a number 0-5 to restrict errors to certain verbosity levels
+        self.verbose = -1 
+        self.parse(cppLintLine)
+
+    def removeSquareBrackets(string):
+        string = string.replace('[','')
+        string = string.replace(']','')
+        return string
+    
+    def parse(self, cppLintLine):
+        (location, self.description, categoryAndVerbose) = cppLintLine.strip().split("  ")
+        self.location = location.split(':')[1]
+        categoryAndVerbose = CppLintError.removeSquareBrackets(categoryAndVerbose)
+        self.category = categoryAndVerbose.split(' ')[0]
+        self.verbose = categoryAndVerbose.split(' ')[1]
+    
+    def output(self):
+        res = '\t'.join([self.location, self.category, self.description, self.verbose])
+        return res
+
+class CppLintFile:
+    def __init__(self, filePath):
+        assert filePath
+        # store cpp files's path, will be needed in future
+        self.path = filePath
+        # extract file name
+        self.fileName = os.path.basename(self.path).split('.')[0]
+        self.errors = []
+        self.content = []
+       
+    def parse(self):
+        for line in self.content:
+            self.errors.append(CppLintError(line))
+
+    def addContent(self, strLine):
+        assert strLine
+        self.content.append(strLine)
+
+    def output(self, path):
+        TEXT_FILE_SUFFIX = '.txt'
+        textFileName = ''.join([self.fileName, TEXT_FILE_SUFFIX])
+        textFilePath = os.path.join(path, textFileName)
+        with open(textFilePath, 'w') as fout:
+            for error in self.errors:
+                fout.write(''.join([error.output(),'\n']))
+        
+class Parser():
+    def __init__(self):
+        self.files = {}
+        self.isParsed = False
+        return
+       
+    def clear(self):
+        self.files.clear()
+        self.isParsed = False
+
+    def parse(self, inputFile):
+        self.clear()
+        # read && store raw data
+        with open(inputFile, 'r') as fin:
+            lines = fin.readlines()
+            for line in lines:
+                fileName = line.split("  ")[0].split(":")[0]
+                if fileName in self.files:
+                    self.files[fileName].addContent(line)
+                else:
+                    self.files[fileName] = CppLintFile(fileName)
+                    self.files[fileName].addContent(line)
+        # real parse implemetion            
+        for lintFile in self.files.values():
+            lintFile.parse()
+        self.isParsed = True
+        return
+
+    def output(self, path):
+        if (self.isParsed == False):
+            print("Parse has not been performed")
+            assert(False)
+        for lintFile in self.files.values():
+            lintFile.output(path)
+        return
